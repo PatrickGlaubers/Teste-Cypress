ba0cc74a3795977791f70196b88f2a903236bcf0
PatrickGlaubers <patrickglauber96@gmail.com>
2025-08-31 18:57:06 -0300

    Subindo teste de edição do endereço de faturamento utilizando arquivos de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,5 @@
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade endereço - Faturamento e entrega', () => {
 
@@ -13,7 +14,24 @@ describe('Funcionalidade endereço - Faturamento e entrega', () => {
   it('Deve fazer cadastro de faturamento com sucesso', () => {
 
     EnderecoPage.editarEnderecoFaturamento('Teste', 'Qualitativo', 'TesteInternacional', 'Brasil', 'Av Paulista', 'Unidade01', 'São Paulo', 'São Paulo', '01310-000', '99999-9999', 'teste_aluno11@teste.com')
+    cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
+  })
+
+  it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados', () => {
+
+    EnderecoPage.editarEnderecoFaturamento(
+      dadosEndereco[1].nome, 
+      dadosEndereco[1].sobrenome, 
+      dadosEndereco[1].empresa, 
+      dadosEndereco[1].pais, 
+      dadosEndereco[1].endereco,
+      dadosEndereco[1].complementar, 
+      dadosEndereco[1].cidade, 
+      dadosEndereco[1].estado,
+      dadosEndereco[1].cep, 
+      dadosEndereco[1].telefone, 
+      dadosEndereco[1].email)
     cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
   })
