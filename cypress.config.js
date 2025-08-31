a3c886c71df76d37a9f17eb898fff2bc1bde2153
PatrickGlaubers <patrickglauber96@gmail.com>
2025-08-31 20:56:57 -0300

    Subindo integração com o Mocha

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   projectId: 'e2ohkm',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
     setupNodeEvents(on, config) {
       
     },
